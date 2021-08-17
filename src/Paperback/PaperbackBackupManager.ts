82003e7fb1d4391c993e5231d5a113aee31ce60b
Framboise <25133304+FramboisePi@users.noreply.github.com>
2021-08-17 11:58:31 +0200

    Add Paperback and Tachiyomi backup managers

---
+++ src/Paperback/PaperbackBackupManager.ts
@@ -0,0 +1,74 @@
+import { PaperbackBackup } from "./PaperbackBackup"
+
+export class PaperbackBackupManager {
+    
+    private backup: PaperbackBackup.Backup
+
+    constructor() {
+        this.loadEmptyBackup()
+    }
+
+    /* Loading a backup */
+
+    loadEmptyBackup() {
+        this.backup = {
+            library:             [],
+            sourceMangas:        [],
+            chapterMarkers:      [],
+            backupSchemaVersion: 3,
+            date:                650282810.89528203,
+            tabs:                [],
+            version:             "v0.6.0-b1.0.5",
+            sourceRepositories:  [],
+            activeSources:       []
+        }
+    }
+
+    load(backup: PaperbackBackup.Backup) {
+        this.backup = backup
+    }
+
+    /* Exporting a backup */
+
+    exportBackup(): PaperbackBackup.Backup {
+        return this.backup
+    }
+
+    /* Helper functions */
+
+    appendLibraryManga(libraryManga: PaperbackBackup.LibraryManga): void {
+        this.backup.library.push(libraryManga)
+    }
+
+    appendSourceManga(sourceManga: PaperbackBackup.SourceManga): void {
+        this.backup.sourceMangas.push(sourceManga)
+    }
+
+    appendChapterMarker(chapterMarker: PaperbackBackup.ChapterMarker): void {
+        this.backup.chapterMarkers.push(chapterMarker)
+    }
+
+    appendChapterMarkers(chapterMarkers: PaperbackBackup.ChapterMarker[]): void {
+        for (const chapterMarker of chapterMarkers) {
+            this.appendChapterMarker(chapterMarker)
+        }
+    }
+
+    appendLibraryTab(tab: PaperbackBackup.LibraryTab): void {
+        this.backup.tabs.push(tab)
+    }
+
+    appendSourceRepository(repository: PaperbackBackup.SourceRepository): void {
+        this.backup.sourceRepositories.push(repository)
+    }
+
+    appendActiveSource(source: PaperbackBackup.ActiveSource): void {
+        // If this source is already registered, do not apply a duplicate
+        if (this.backup.activeSources.some(x => source.id === x.id)) {
+            return
+        }
+
+        this.backup.activeSources.push(source)
+    }
+
+}
---
+++ src/Tachiyomi/TachiyomiBackupManager.ts
@@ -0,0 +1,80 @@
+import { Reader } from "protobufjs";
+import { TachiyomiObjectModel } from "./proto/TachiyomiObjectModel"
+
+const pako = require('pako');
+
+export class TachiyomiBackupManager {
+    
+    // We don't use the IBackup interface but the Backup class.
+    private backup: TachiyomiObjectModel.Backup
+
+    constructor() {
+        this.loadEmptyBackup()
+    }
+
+    /* Loading a backup */
+
+    loadEmptyBackup() {
+        this.backup = new TachiyomiObjectModel.Backup()
+    }
+
+    loadBackup(backup: TachiyomiObjectModel.Backup) {
+        this.backup = backup
+    }
+
+    loadIBackup(iBackup: TachiyomiObjectModel.IBackup) {
+        const backup = new TachiyomiObjectModel.Backup(iBackup)
+        this.loadBackup(backup)
+    }
+
+
+    loadProto(protoBackup: Reader | Uint8Array) {
+        const backup = TachiyomiObjectModel.Backup.decode(protoBackup)
+        this.loadBackup(backup)
+    }
+
+    loadProtoGz(protoGzBackup) {
+        const protoBackup = pako.inflate(protoGzBackup)
+        this.loadProto(protoBackup)
+    }
+
+    /* Exporting a backup */
+    
+    exportBackup(): TachiyomiObjectModel.Backup {
+        return this.backup
+    }
+
+    exportIBackup() {
+        return TachiyomiObjectModel.Backup.toObject(this.backup)
+    }
+
+    exportProto(): Uint8Array {
+        // WARNING including non empty backupManga.categories result in an error
+        // "Expecting wire type 0 byt found 2"
+
+        const writer = TachiyomiObjectModel.Backup.encode(this.backup)
+        // We must call finish() to obtain an usable Buffer
+        // https://github.com/protobufjs/protobuf.js/#using-proto-files
+        const buffer = writer.finish()  
+        return buffer
+    }
+
+    exportProtoGz() {
+        const protoBackup = this.exportProto()
+        const protoGzBackup = pako.gzip(protoBackup)
+        return protoGzBackup
+    }
+
+    /* Helper functions */
+
+    appendBackupManga(backupManga: TachiyomiObjectModel.IBackupManga): void {
+        const backupMangaObject = TachiyomiObjectModel.BackupManga.create(backupManga)
+        this.backup.backupManga.push(backupMangaObject)
+    }
+
+    appendCategory(category: TachiyomiObjectModel.IBackupCategory): void {
+        const categoryObject = TachiyomiObjectModel.BackupCategory.create(category)
+        this.backup.backupCategories.push(categoryObject)
+    }
+
+}
