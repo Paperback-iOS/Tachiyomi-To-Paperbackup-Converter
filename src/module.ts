40b85875b49d8d43f90cabea2a2abd00b4d385bb
Framboise <25133304+FramboisePi@users.noreply.github.com>
2021-08-22 17:58:21 +0200

    Add a module

---
+++ src/module.ts
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+export * from './Tachiyomi/TachiyomiBackupManager'
+export * from './Paperback/PaperbackBackupManager'
+export * from './BackupConverters/TachiToPaperBackupConverter'
+export * from './BackupConverters/PaperToTachiBackupConverter'
